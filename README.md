fd6a5e576450be84d956a3ac3938b5237d31d6e7
Parker Miller <despondentdonkey@gmail.com>
2014-07-18 14:52:09 -0600

    Add loadtime.lua and savetime.lua

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ vlc-timekeeper
 ==============
 
 Save current time and load saved time in VLC.
+
+Only tested on version 2.0.6
+
+Place lua files in VLC_HOME/lua/extensions/ then in VLC you'll find ```Save Time``` and ```Load Time``` commands in the View menu.
---
+++ savetime.lua
@@ -0,0 +1,36 @@
+-- Extension description
+function descriptor()
+  return {
+    title = "Save Time";
+    version = "1.0";
+    author = "Parker Miller";
+    shortdesc = "Save elapsed time.";
+    description = "Logs the current video time in the video directory.";
+  }
+end
+
+function activate()
+  local input = vlc.object.input()
+  if input then
+    local elapsed = vlc.var.get(input, "time")
+    local minutes = math.floor((elapsed % 3600) / 60)
+    local seconds = math.floor(elapsed % 60)
+
+    if (seconds < 10) then
+      seconds = "0" .. seconds
+    end
+
+    if (minutes < 10) then
+      minutes = "0" .. minutes
+    end
+
+    local uri = vlc.strings.decode_uri(vlc.input.item():uri()) -- Format: file:///dir1/dir2/file.ext
+    local filePath = string.gsub(uri, "file:///", "")          -- Format: dir1/dir2/file.ext
+    local directory = string.gsub(filePath, "/[^/]*$", "")     -- Format: dir1/dir2         Remove last / and everything after it to leave the directory.
+
+    io.output(directory .. "/" .. vlc.input.item():name() .. ".txt")
+    io.write(elapsed .. "\n" .. minutes .. ":" .. seconds)
+    io.close()
+  end
+  deactivate()
+end
